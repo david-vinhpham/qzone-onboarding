4c4a84263bd8fd93ffee5a6e205a75cfaf5b0250
Amit Agarwal <amit.agarwal@ongraph.com>
2018-07-20 12:37:20 +0530

    Sidebar added

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,76 @@
+// @material-ui/icons
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import ContentPaste from "@material-ui/icons/ContentPaste";
+import BusinessCenter from "@material-ui/icons/BusinessCenter";
+import Report from "@material-ui/icons/Report";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+import Unarchive from "@material-ui/icons/Unarchive";
+// core components/views
+import Dashboard from "views/Dashboard/Dashboard.jsx";
+import Administration from "views/Administration/Administration.jsx";
+import BusinessDetails from "views/BusinessDetails/BusinessDetails.jsx";
+import Calendar from "views/Calendar/Calendar.jsx";
+import CustomerFlow from "views/CustomerFlow/CustomerFlow.jsx";
+import Provider from "views/Provider/Provider.jsx";
+import Reports from "views/Reports/Reports.jsx";
+import Services from "views/Services/Services.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: DashboardIcon,
+    component: Dashboard    
+  },
+  {
+    path: "/customer_flow",
+    name: "Manage Customer Flow",
+    icon: DashboardIcon,
+    component: CustomerFlow    
+  },
+  {
+    path: "/business_details",
+    name: "Update Business Details",
+    icon: BusinessCenter,
+    component: BusinessDetails
+  },
+  {
+    path: "/services",
+    name: "Manage Services",
+    icon: ContentPaste,
+    component: Services
+  },
+  {
+    path: "/reports",
+    name: "Reports",
+    icon: Report,
+    component: Reports
+  },
+  {
+    path: "/calendar",
+    name: "Manage Calendar",
+    icon: BubbleChart,
+    component: Calendar
+  },
+  {
+    path: "/administration",
+    name: "Administration",
+    icon: LocationOn,
+    component: Administration
+
+  },
+  {
+    path: "/provider/list",
+    name: "Provider Details",
+    icon: Person,
+    component: Provider
+  },
+  { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
+  
+];
+
+export default dashboardRoutes;
---
+++ src/views/Provider/Provider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Provider extends React.Component{
+	render() {
+		return(
+			<h1>provider</h1>
+		)
+	}
+}
