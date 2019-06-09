f141ab13ee60a8443d3f9e9b9c32bda395a9da92
DavidPham <david.vinhphamthanh@gmail.com>
2019-06-09 14:07:46 +0700

    [SURVEY] routing to Assessment

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,5 +1,8 @@
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import { BubbleChart, BusinessCenter, Email, LocationOn, Person, PersonAdd, Report } from '@material-ui/icons';
+import {
+  BubbleChart, BusinessCenter, Email, LocationOn, Person, PersonAdd, Report,
+  Assignment,
+} from '@material-ui/icons';
 import Dashboard from '../views/Dashboard/Dashboard';
 import Profile from '../views/Profile/profile';
 import Administration from '../views/Administration/Administration';
@@ -36,6 +39,7 @@ import ServiceCategoriesList from '../views/ServiceCategories/ServiceCategoriesL
 import BusinessCategoriesList from '../views/BusinessCategories/BusinessCategoriesList';
 import TmpServicesDetail from '../views/TmpServices/TmpServicesDetail';
 import AvailabilitySlotsList from '../views/AvailabilitySlots/AvailabilitySlotsList';
+import Assessments from '../views/Surveys/Assessments';
 
 export const dashboardRoutes = [
   {
@@ -168,6 +172,12 @@ export const otherRoutes = [
     name: 'Administration',
     icon: LocationOn,
     component: Administration
+  },
+  {
+    path: '/assessments',
+    name: 'Assessments',
+    icon: Assignment,
+    component: Assessments,
   },
   {
     path: '/provider/list',
---
+++ src/views/Surveys/Assessments.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Survey extends Component {
+  render() {
+    return (
+      <div>
+        SURVEY App
+      </div>
+    )
+  }
+}
+
+export default Survey;
