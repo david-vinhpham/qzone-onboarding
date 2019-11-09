74b39507b4135ac6dd35f33a1884e0dbe733a52e
Nhat Dang <dangquangnhat18101994@gmail.com>
2019-11-09 11:40:52 +0700

    Apply react lazy

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react';
+import React, { createRef, Suspense } from 'react';
 import { connect } from 'react-redux';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
@@ -107,23 +107,25 @@ class Dashboard extends React.PureComponent {
           />
           <div className={classes.content}>
             <div className={classes.container}>
-              <Switch>
-                <Route
-                  path={profileRouteComponent.path}
-                  component={withAuth(profileRouteComponent.component, rest.userDetail)}
-                  key={profileRouteComponent.path}
-                />
-                {!!axios.defaults.headers.common['Authorization'] &&
-                  [
-                    ...otherRoutes,
-                    ...managementRoutes,
-                    ...operationRoutes,
-                    ...dashboardRoutes,
-                  ].map((prop) => {
-                    if (prop.redirect) return <Redirect from={prop.path} to={prop.pathTo} key={prop.path} />;
-                    return <Route path={prop.path} component={withAuth(prop.component, rest.userDetail)} key={prop.path} />;
-                  })}
-              </Switch>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route
+                    path={profileRouteComponent.path}
+                    component={withAuth(profileRouteComponent.component, rest.userDetail)}
+                    key={profileRouteComponent.path}
+                  />
+                  {!!axios.defaults.headers.common['Authorization'] &&
+                    [
+                      ...otherRoutes,
+                      ...managementRoutes,
+                      ...operationRoutes,
+                      ...dashboardRoutes,
+                    ].map((prop) => {
+                      if (prop.redirect) return <Redirect from={prop.path} to={prop.pathTo} key={prop.path} />;
+                      return <Route path={prop.path} component={withAuth(prop.component, rest.userDetail)} key={prop.path} />;
+                    })}
+                </Switch>
+              </Suspense>
             </div>
           </div>
         </div>
--- src/views/Administration/Administration.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Administration extends React.Component {
-  render() {
-    return <h1>Administration</h1>;
-  }
-}
